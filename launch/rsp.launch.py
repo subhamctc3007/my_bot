ae146ede0335213ebadc37807d9c6b724c29e963
subhamctc3007 <subhamctc3007@gmail.com>
2025-08-14 18:33:37 +0530

    LiDAR added with a custom world

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -29,13 +29,10 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
     # Launch!
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-
-        node_robot_state_publisher
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument('use_sim_time', default_value='false', description='Use sim time if true'),
+            node_robot_state_publisher
+        ]
+    )
